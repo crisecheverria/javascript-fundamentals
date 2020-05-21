ce7b3d2547477e657a2c29554eb861c7c9fa47e8
Cristian <cristian.echeverri4@gmail.com>
2020-05-21 17:58:37 +0200

    Frequency

--- exercises/frequency.js
+++ exercises/frequency.js
@@ -2,3 +2,17 @@
 frequency(['a', 'b', 'b', 'c', 'b', 'a']) # => ['b', 'a', 'c']
 order the items by high to low ocurrency
 */
+
+function frequency(arr) {
+  let obj = {};
+
+  arr.forEach((item) => {
+    obj[item] = obj[item] ? obj[item] + 1 : 1;
+  });
+
+  return Object.keys(obj).sort(function (a, b) {
+    return obj[b] - obj[a];
+  });
+}
+
+frequency(['a', 'b', 'b', 'c', 'b', 'a']);
